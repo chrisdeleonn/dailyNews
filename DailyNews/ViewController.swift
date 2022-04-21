f2bf85e4f5637bfa28623d3a07f1e5fe9c3e0d45
Chris De Leon <cdeleon.ca91@gmail.com>
2022-04-20 22:44:57 -0400

    Initial Commit

---
+++ DailyNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyNews
+//
+//  Created by Chris De Leon on 4/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
