c7c74a13088055bbef293a381668cd8b42e11ac2
Chris De Leon <cdeleon.ca91@gmail.com>
2022-04-21 10:01:32 -0400

    added tableViews

--- DailyNews/ViewController.swift
+++ DailyNews/ViewController.swift
@@ -7,25 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self,
+                       forCellReuseIdentifier: NewsTableViewCell.identifier)
+        return table
+    }()
+    
+    private var viewModels = [NewsTableViewCellViewModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "News"
+        view.addSubview(tableView)
         view.backgroundColor = .systemBackground
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        APICaller.shared.getTopStories { result in
+        
+        APICaller.shared.getTopStories { [weak self] result in
             switch result {
-            case .success(let response):
-                break
+            case .success(let articles):
+                
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(title: $0.title,
+                    subtitle: $0.description ?? "No description",
+                    imageURL: URL(string: $0.urlToImage ?? "")
+                    )
+                })
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
             case .failure(let error):
                 print(error)
             }
-            
         }
-        
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+//Table
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NewsTableViewCell.identifier,
+            for: indexPath
+            
+        ) as? NewsTableViewCell else {
+            fatalError()
+        }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
 
 }
 
